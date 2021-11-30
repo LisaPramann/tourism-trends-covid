0af95ed031fcf2baef6e2b73dd2671701ab16a32
LisaPramann <lisa.pramann@gmail.com>
2021-11-30 22:40:44 +0100

    glimpse of data set, failed to download the right one from eurostat

---
+++ 98_data-wrangling-scrips/glimpse_eurpstat_data.R
@@ -0,0 +1,7 @@
+#Understanding the Eurostat Data Set
+
+library(foreign)
+library(tidyverse)
+
+data_eurostat <- read.csv("./99_data-sets-raw/eurostat_tour_linear.csv")
+
--- 98_data-wrangling-scrips/glimpse_park_data.R
+++ 98_data-wrangling-scrips/glimpse_park_data.R
@@ -7,4 +7,4 @@ data_parks <- read.csv("./99_data-sets-raw/change-visitors-parks-covid-raw.csv")
 
 #Problem: Data shows how the number of visitors to parks and outdoor spaces has changed compared 
 #to baseline days (the median value for the 5‑week period from January 3 to February 6, 2020).
-#Question: Can we change that?
+#Question: Can we change that? https://ourworldindata.org/covid-google-mobility-trends 
