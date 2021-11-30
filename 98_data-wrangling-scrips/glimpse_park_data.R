29a669d68a12bd8d1aa7001aba9ecb0b3f56c2d8
LisaPramann <lisa.pramann@gmail.com>
2021-11-30 22:03:23 +0100

    Understanding the first data set

---
+++ 98_data-wrangling-scrips/glimpse_park_data.R
@@ -0,0 +1,10 @@
+#Understanding the World in Data Park Data Set 
+
+library(foreign)
+library(tidyverse)
+
+data_parks <- read.csv("./99_data-sets-raw/change-visitors-parks-covid-raw.csv")
+
+#Problem: Data shows how the number of visitors to parks and outdoor spaces has changed compared 
+#to baseline days (the median value for the 5‑week period from January 3 to February 6, 2020).
+#Question: Can we change that?
