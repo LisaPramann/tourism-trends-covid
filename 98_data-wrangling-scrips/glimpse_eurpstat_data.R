93822a8689ff9d082fb8dc77bcf82b0b2e5bebd6
LisaPramann <lisa.pramann@gmail.com>
2021-12-02 22:57:43 +0100

    importet different Eurostat sets

--- 98_data-wrangling-scrips/glimpse_eurpstat_data.R
+++ 98_data-wrangling-scrips/glimpse_eurpstat_data.R
@@ -2,6 +2,13 @@
 
 library(foreign)
 library(tidyverse)
+#install.packages("readxl")
+library(readxl)
 
-data_eurostat <- read.csv("./99_data-sets-raw/eurostat_tour_linear.csv")
+eurostat_total <- read_excel("./99_data-sets-raw/Eurostat/eurostat-total.xlsx")
+eurostat_domestic <- read_excel("./99_data-sets-raw/Eurostat/eurostat-domestic.xlsx")
+eurostat_foreign <- read_excel("./99_data-sets-raw/Eurostat/eurostat-foreign.xlsx")
 
+eurostat_total_camping <- read_excel("./99_data-sets-raw/Eurostat/eurostat-total-camping.xlsx")
+eurostat_domestic_camping <- read_excel("./99_data-sets-raw/Eurostat/eurostat-domestic-camping.xlsx")
+eurostat_domestic_hotel <- read_excel("./99_data-sets-raw/Eurostat/eurostat-domestic-hotels.xlsx")
