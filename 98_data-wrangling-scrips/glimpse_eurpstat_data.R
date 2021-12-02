e80f2c1fa3598b391cb11d797a4e040f01ff912b
LisaPramann <lisa.pramann@gmail.com>
2021-12-02 23:00:08 +0100

    add first trends

--- 98_data-wrangling-scrips/glimpse_eurpstat_data.R
+++ 98_data-wrangling-scrips/glimpse_eurpstat_data.R
@@ -12,3 +12,6 @@ eurostat_foreign <- read_excel("./99_data-sets-raw/Eurostat/eurostat-foreign.xls
 eurostat_total_camping <- read_excel("./99_data-sets-raw/Eurostat/eurostat-total-camping.xlsx")
 eurostat_domestic_camping <- read_excel("./99_data-sets-raw/Eurostat/eurostat-domestic-camping.xlsx")
 eurostat_domestic_hotel <- read_excel("./99_data-sets-raw/Eurostat/eurostat-domestic-hotels.xlsx")
+
+#First Trends: Generally domestic tourism is the same in July/August 2019 compared to 2020/21 for some countries even increasing 
+#Domestic Tourists increasingly stayed ad campsites in July/August 2019 compared to 2020/21
