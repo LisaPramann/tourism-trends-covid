d3fca34ee06a4be98b1b6d54f526f0bbc92db4cc
LisaPramann <lisa.pramann@gmail.com>
2021-12-10 08:29:31 +0100

    add Covid Stats

--- 98_data-wrangling-scripts/glimpse_covid_measures_index_europe.R
+++ 98_data-wrangling-scripts/glimpse_covid_measures_index_europe.R
@@ -9,14 +9,27 @@ Europe_iso <- c("AUT", "BEL", "BGR", "HRV", "HUN", "LVA", "FRA", "FIN", "GRC", "
                 "DNK", "CZE", "EST", "DEU","ESP","ROU", "NLD", "POL", "PRT", "IRL", 
                 "LTU", "LUX", "ITA", "MLT", "SWE", "SVN", "SVK")
 
-data_covid_measures_raw <- read.csv("./99_data-sets-raw/covid-stringency-index.csv") %>% 
-  filter(Code == Europe_iso)
- 
-data_covid_measures <- data_covid_measures_raw %>%
+data_covid_measures <- read.csv("./99_data-sets-raw/covid-stringency-index.csv") %>% 
+  filter(Code == Europe_iso) %>%
   mutate(Month = format(ymd(Day), "%Y-%m")) %>%
   group_by(Month) %>% 
-  summarize(mean_month = mean(stringency_index)) %>%
+  summarize(mean_month_measures = mean(stringency_index)) %>%
+  arrange(Month) %>%
+  mutate(Month = lubridate::ym(Month))
+ 
+data_covid_cases <-  read.csv("./99_data-sets-raw/owid-covid-data.csv") %>% 
+  filter(iso_code == Europe_iso) %>%
+  mutate(Month = format(ymd(date), "%Y-%m")) %>%
+  group_by(Month) %>% 
+  summarize(mean_month_cases = mean(new_cases_per_million)) %>%
   arrange(Month) %>%
   mutate(Month = lubridate::ym(Month))
 
+
+#Join the two data sets of from our Wold in data to create Covid statistics 
+covid_stats <- left_join( 
+  data_covid_measures, data_covid_cases)
+
 # Run source("98_data-wrangling-scripts/glimpse_covid_measures_index_europe.R") in the file you want to import the clean data to.
+
+  
