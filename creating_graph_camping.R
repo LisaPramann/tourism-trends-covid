c616ad45d54d6cd78207954501c80b58ba92a1fc
Kathryn Malchow <78643030+KathrynMalchow@users.noreply.github.com>
2021-12-16 12:59:54 +0100

    Removed Iceland from camping graph

--- creating_graph_camping.R
+++ creating_graph_camping.R
@@ -7,7 +7,6 @@ source("98_data-wrangling-scripts/glimpse_eurostat_data.R")
 
 sum_dom_camp_long = pivot_longer(summer_domestic_camping_perc, !States, names_to = "date", values_to = "value")
 
-camp_dom_long = pivot_longer(eurostat_domestic_camping_percent, !States, names_to = "date", values_to = "value")
 
 #version 1
 sum_dom_camp_long %>% 
@@ -34,7 +33,7 @@ sum_dom_camp_long %>%
   group_by(States) %>% 
   mutate("average" = mean(value)) %>% 
   filter_at(vars(contains("States")), all_vars(! . %in% c("Euro area","European Union - 27 countries (from 2020)","European Union - 28 countries (2013-2020)","Malta", 
-  "United Kingdom","Liechtenstein","Norway", "Switzerland", "Cyprus"))) %>%  
+  "United Kingdom","Liechtenstein","Norway", "Switzerland", "Cyprus", "Iceland"))) %>%  
 ggplot(mapping = aes(x = reorder(States, -average), y = average, fill = average)) +
   geom_col(position = "identity") + #have to specify identity or dodge otherwise it will add all the same average values together per country
   scale_y_continuous(labels = function(x) paste0(x,"%"))+
