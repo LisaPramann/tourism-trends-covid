2dc4ec359ef48420ab3d335918e2fe941bc14918
LisaPramann <lisa.pramann@gmail.com>
2021-12-18 19:01:57 +0100

    cleaned the code

--- 97_data_visualisation_scripts/creating_graph_camping.R
+++ 97_data_visualisation_scripts/creating_graph_camping.R
@@ -5,27 +5,55 @@ pacman::p_load(foreign, tidyverse, readxl, ggplot2, ggridges, hrbrthemes)
 source("./98_data-wrangling-scripts/glimpse_eurostat_data.R")
 
 
-sum_dom_camp_long = pivot_longer(summer_domestic_camping_perc, !States, names_to = "date", values_to = "value")
+sum_dom_camp_long = pivot_longer(summer_domestic_camping_perc,
+                                 !States,
+                                 names_to = "date",
+                                 values_to = "value")
 
 
-sum_dom_camp_long %>% 
-  na.omit() %>% 
-  group_by(States) %>% 
-  mutate("average" = mean(value)) %>% 
-  filter_at(vars(contains("States")), all_vars(! . %in% c("Euro area","European Union - 27 countries (from 2020)","European Union - 28 countries (2013-2020)","Malta", 
-  "United Kingdom","Liechtenstein","Norway", "Switzerland", "Cyprus", "Iceland"))) %>%  
-ggplot(mapping = aes(x = reorder(States, -average), y = average, fill = average)) +
+sum_dom_camp_long %>%
+  na.omit() %>%
+  group_by(States) %>%
+  mutate("average" = mean(value)) %>%
+  filter_at(vars(contains("States")), all_vars(
+    !. %in% c(
+      "Euro area",
+      "European Union - 27 countries (from 2020)",
+      "European Union - 28 countries (2013-2020)",
+      "Malta",
+      "United Kingdom",
+      "Liechtenstein",
+      "Norway",
+      "Switzerland",
+      "Cyprus",
+      "Iceland"
+    )
+  )) %>%
+  ggplot(mapping = aes(
+    x = reorder(States,-average),
+    y = average,
+    fill = average
+  )) +
   geom_col(position = "identity") + #have to specify identity or dodge otherwise it will add all the same average values together per country
-  scale_y_continuous(labels = function(x) paste0(x,"%"))+
-  scale_fill_viridis_c(option = "mako", begin = 0.3, end = 0.9)+
-  labs(x = "Country", y = "Average Change", caption = "Source: Eurostat, 2021")+
+  scale_y_continuous(
+    labels = function(x)
+      paste0(x, "%")
+  ) +
+  scale_fill_viridis_c(option = "mako",
+                       begin = 0.3,
+                       end = 0.9) +
+  labs(x = "Country", y = "Average Change", caption = "Source: Eurostat, 2021") +
   theme_ipsum() +
-  guides(fill=guide_legend(title= "Percentage"))+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.y = element_text(hjust = 0.5),
-        axis.title.x = element_text(hjust = 0.5))
+  guides(fill = guide_legend(title = "Percentage")) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.title.y = element_text(hjust = 0.5),
+    axis.title.x = element_text(hjust = 0.5)
+  )
 
-ggsave(file = "./graphs/camping_domestic.png", width = 9, height = 5)
+ggsave(file = "./graphs/camping_domestic.png",
+       width = 9,
+       height = 5)
 
 
 #title = "Summer Camping Rates for 2020/2021 as Compared to 2019"  
--- 97_data_visualisation_scripts/park_visits_graph_script.R
+++ 97_data_visualisation_scripts/park_visits_graph_script.R
@@ -16,26 +16,34 @@ source("./98_data-wrangling-scripts/glimpse_park_data.R")
 eu_parks_mean <- eu_parks %>%
   group_by(Days) %>%
   summarize(Visits = mean(parks)) %>%
-  mutate_at(2, round, 2) %>% 
+  mutate_at(2, round, 2) %>%
   mutate(Days = lubridate::ymd(Days))
 
 
 
-freq_parks_visit <- eu_parks_mean %>% 
-  ggplot(aes (x = Days, y = Visits)) + 
-  geom_line(size = 1.1, color ="#404788FF") + 
+freq_parks_visit <- eu_parks_mean %>%
+  ggplot(aes (x = Days, y = Visits)) +
+  geom_line(size = 1.1, color = "#404788FF") +
   geom_area(fill = "#20A387FF", alpha = 0.5) +
-  scale_y_continuous(limits = c(-40, 175), 
-                     breaks = c(-25, 0, 25, 50, 75, 100, 125, 150),
-                     labels = function(x) paste0(x, "%"),
-                     name = "Average Change of Visits") + 
-  scale_x_date(limits = as.Date(c("2020-03-01", "2021-09-01")), 
-               date_breaks = "2 month", date_minor_breaks = "month", 
-               date_labels = "%m-%Y", name = "Months of the Pandemic (March 2020 - September 2021)") + 
-  theme_minimal() + 
-  theme(axis.title.x = element_text(margin = margin(t = 15), size = 10),
-        axis.title.y = element_text(size = 10),
-  ) + 
+  scale_y_continuous(
+    limits = c(-40, 175),
+    breaks = c(-25, 0, 25, 50, 75, 100, 125, 150),
+    labels = function(x)
+      paste0(x, "%"),
+    name = "Average Change of Visits"
+  ) +
+  scale_x_date(
+    limits = as.Date(c("2020-03-01", "2021-09-01")),
+    date_breaks = "2 month",
+    date_minor_breaks = "month",
+    date_labels = "%m-%Y",
+    name = "Months of the Pandemic (March 2020 - September 2021)"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.title.x = element_text(margin = margin(t = 15), size = 10),
+    axis.title.y = element_text(size = 10),
+  ) +
   labs(caption = "Source: Our World in Data, 2021")
 
 
